cd5fb3b718631b2dac3c0980af5e1759fd118b7b
Stepan Radionov <radiostep@rambler.ru>
2018-02-20 13:31:06 +0000

    flask works

--- geo.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
-
-@app.route('/')
-def geo():
-    cssUrl = url_for('static', filename='style.css')
-    return render_template('index.html', cssUrl = cssUrl)
